ba536d13a2ba06d2b6ce82990700e6c026ebe017
Jungho Lee <ljh2246@gmail.com>
2021-10-04 01:53:35 +0900

    metric, input size

--- src/weather.js
+++ src/weather.js
@@ -4,7 +4,7 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
     fetch(url).then(response => response.json()).then(data => {
         const city = document.querySelector("#weather span:first-child");
